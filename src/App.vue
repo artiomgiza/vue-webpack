67d2bcada5a6cfc6d2da1db4e70e7b68c6f405db
Artiom Giza <artiomg@gettaxi.com>
2018-06-14 00:22:07 +0300

    navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <app-message></app-message>
+    <h1>Let's go somwhere!</h1>
+    <p>Finished: academind <a href="https://www.youtube.com/playlist?list=PL55RiY5tL51p-YU-Uw90qQH419BM4Iz07">Vue.js 2 Getting Started course</a></p>
+    <hr>
+    <router-link to="/">Home</router-link>
+    <router-link to="/users/10">Users10</router-link>
+    <router-link to="/users/12">Users12</router-link>
+    <hr>
+
+    <router-view></router-view>
   </div>
 </template>
 
